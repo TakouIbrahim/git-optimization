95ff4fe1aa1e026bec4e24f65a9600b1b72ce3e6
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2023-07-11 12:39:43 +0200

    Init code

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+OK. here is a little bit of code to get you started.
+ */
+var a = 1;
+var b = "11";
+var c = a + b;
+console.log("Int + String in JS :" + c);
+console.log("Int - String in JS :" + (a - b));
+console.log("Int * String in JS :" + (a * b));
+console.log("Int / String in JS :" + (a / b));
+// Path: main.js
